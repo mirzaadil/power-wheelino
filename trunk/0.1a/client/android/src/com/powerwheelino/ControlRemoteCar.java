cb33793948d5341e342597e86b60c5e312c734c0
kranzrm <kranzrm@2fc9d784-2164-c497-5448-e10d180b2835>
2012-07-02 03:43:53 +0000

    Actual code.... hopefully

---
+++ trunk/0.1a/client/android/src/com/powerwheelino/ControlRemoteCar.java
@@ -0,0 +1,30 @@
+package com.powerwheelino;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class ControlRemoteCar extends Activity {
+    /** Called when the activity is first created. */
+	MyView mainview; 
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        JoystickView joystick = new JoystickView(this);
+        setContentView(joystick);
+        //mainview = new MyView(this,12345,"192.168.1.101");
+        //setContentView(mainview);
+        
+    }
+    
+
+
+}
+
+//Paint paint = new Paint(); 
+//paint.setColor(Color.WHITE); 
+//paint.setStyle(Style.FILL); 
+//canvas.drawPaint(paint); 
+//
+//paint.setColor(Color.BLACK); 
+//paint.setTextSize(20); 
+//canvas.drawText("Some Text", 10, 25, paint); 
---
+++ trunk/0.1a/client/android/src/com/powerwheelino/JoystickClickedListener.java
@@ -0,0 +1,6 @@
+package com.powerwheelino;
+
+public interface JoystickClickedListener {
+        public void OnClicked();
+        public void OnReleased();
+}
