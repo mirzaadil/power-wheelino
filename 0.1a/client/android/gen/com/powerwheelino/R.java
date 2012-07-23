7b5bbc618b95e5339b3f4193d684db6c1a88eb54
kranzrm <kranzrm@2fc9d784-2164-c497-5448-e10d180b2835>
2012-07-23 01:34:25 +0000

    Updated Android Client Settings Menu (Debug and Rate)

--- 0.1a/client/android/gen/com/powerwheelino/R.java
+++ 0.1a/client/android/gen/com/powerwheelino/R.java
@@ -13,11 +13,35 @@ public final class R {
     public static final class drawable {
         public static final int ic_launcher=0x7f020000;
     }
+    public static final class id {
+        public static final int RelativeLayout1=0x7f060000;
+        public static final int TextView01=0x7f060008;
+        public static final int TextView02=0x7f060007;
+        public static final int TextView03=0x7f06000a;
+        public static final int button1=0x7f06000b;
+        public static final int cancelbutton=0x7f060003;
+        public static final int editText1=0x7f060001;
+        public static final int item1=0x7f06000d;
+        public static final int lightToggle=0x7f06000c;
+        public static final int rateBar1=0x7f060006;
+        public static final int savebutton=0x7f060004;
+        public static final int serverPort=0x7f060002;
+        public static final int textView1=0x7f060005;
+        public static final int toggleDbgBtn=0x7f060009;
+    }
     public static final class layout {
         public static final int main=0x7f030000;
+        public static final int settingslayout=0x7f030001;
+        public static final int tablelayout=0x7f030002;
+        public static final int togglebuttonlayout=0x7f030003;
+    }
+    public static final class menu {
+        public static final int settingsmenu=0x7f050000;
     }
     public static final class string {
+        public static final int SaveButtonText=0x7f040003;
         public static final int app_name=0x7f040001;
         public static final int hello=0x7f040000;
+        public static final int ipSettingsLabel=0x7f040002;
     }
 }
