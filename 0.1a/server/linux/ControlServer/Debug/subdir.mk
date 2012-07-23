3dc35eb71bdc6c51887ea779e4f93790732c12f4
kranzrm <kranzrm@2fc9d784-2164-c497-5448-e10d180b2835>
2012-07-23 01:38:59 +0000

    Implemented threads in Control Server ( must fix serial write )

--- 0.1a/server/linux/ControlServer/Debug/subdir.mk
+++ 0.1a/server/linux/ControlServer/Debug/subdir.mk
@@ -14,10 +14,10 @@ CPP_DEPS += \
 
 
 # Each subdirectory must supply rules for building sources it contributes
-%.o: ../%.cpp
+ControlServer.o: ../ControlServer.cpp
 	@echo 'Building file: $<'
 	@echo 'Invoking: GCC C++ Compiler'
-	g++ -Iserial -O0 -g3 -Wall -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)" -o "$@" "$<"
+	g++ -O0 -g3 -Wall -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"ControlServer.d" -o "$@" "$<"
 	@echo 'Finished building: $<'
 	@echo ' '
 
