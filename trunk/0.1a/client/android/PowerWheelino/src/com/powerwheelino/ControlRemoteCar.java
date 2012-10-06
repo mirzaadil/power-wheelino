f8b258520363c493518821b41ac5782b527ce06a
kranzrm <kranzrm@2fc9d784-2164-c497-5448-e10d180b2835>
2012-10-06 20:38:52 +0000

    attempted removal of Android client extra directories

--- trunk/0.1a/client/android/PowerWheelino/src/com/powerwheelino/ControlRemoteCar.java
+++ trunk/0.1a/client/android/PowerWheelino/src/com/powerwheelino/ControlRemoteCar.java
@@ -19,6 +19,8 @@ public class ControlRemoteCar extends Activity {
 	public static final String PREFS_NAME = "Preferences";
 	private String serverIP;
 	private int serverPort;
+	private int messageRate;
+	private boolean debugMode;
 	
 	//View Variables for later access
 	private JoystickView joystick;
@@ -31,7 +33,7 @@ public class ControlRemoteCar extends Activity {
 	private Handler handler;
 	private Thread commThread;
 	private boolean  paused = false;
-	private static final long MSG_INTERVAL = 20; //Milliseconds
+	private static long MSG_INTERVAL = 20; //Milliseconds
 	
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -55,10 +57,16 @@ public class ControlRemoteCar extends Activity {
                 setContentView(R.layout.settingslayout);
                 
                 EditText ipTextBox = (EditText)findViewById(R.id.editText1);
-                EditText portTextBox = (EditText)findViewById(R.id.serverPort);
-                
                 ipTextBox.setText(serverIP, TextView.BufferType.EDITABLE);
+                
+                EditText portTextBox = (EditText)findViewById(R.id.serverPort);
                 portTextBox.setText(String.valueOf(serverPort));
+                
+                SeekBar msgRateBar = (SeekBar)findViewById(R.id.rateBar1);
+                msgRateBar.setProgress(messageRate);
+                
+                ToggleButton dbgBtn = (ToggleButton)findViewById(R.id.toggleDbgBtn);
+                dbgBtn.setChecked(debugMode);
                 
                 return true;
             default:
@@ -74,9 +82,17 @@ public class ControlRemoteCar extends Activity {
     	EditText srvPrt = (EditText)findViewById(R.id.serverPort);
     	int srvPortInt = Integer.parseInt(srvPrt.getText().toString());
     	
+    	SeekBar msgRateBar = (SeekBar)findViewById(R.id.rateBar1);
+    	int msgRate = msgRateBar.getProgress();
+    	
+    	ToggleButton dbgBtn = (ToggleButton)findViewById(R.id.toggleDbgBtn);
+    	boolean debugYN = dbgBtn.isChecked();
+    	
     	SharedPreferences settings = getSharedPreferences(PREFS_NAME, 0);
     	SharedPreferences.Editor editor = settings.edit();
     	
+    	editor.putInt("messageRate", msgRate);
+    	editor.putBoolean("debugMode", debugYN);
     	editor.putString("serverIP", ipAddrString);
         editor.putInt("serverPort", srvPortInt);
         editor.commit();
@@ -93,6 +109,10 @@ public class ControlRemoteCar extends Activity {
         SharedPreferences settings = getSharedPreferences(PREFS_NAME, 0);
         serverIP = settings.getString("serverIP", "192.168.1.1");
         serverPort = settings.getInt("serverPort", 50000);
+        messageRate = settings.getInt("messageRate", 50);
+        debugMode = settings.getBoolean("debugMode", false);
+        
+        MSG_INTERVAL = messageRate;
         
         setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
         joystick = new JoystickView(this);
@@ -102,10 +122,10 @@ public class ControlRemoteCar extends Activity {
         udp = new UdpClient(serverIP, serverPort);
         if (udp.isConnected()){
         	CharSequence tMsg = serverIP + "@" + serverPort + " - Connected Successfully !!!";
-//        	Toast.makeText(this.getApplicationContext(),tMsg , Toast.LENGTH_SHORT).show();
+        	Toast.makeText(this.getApplicationContext(),tMsg , Toast.LENGTH_SHORT).show();
         } else {
         	CharSequence tMsg = "Server " + serverIP + "@" + serverPort + " not connected !!!";
-//        	Toast.makeText(this.getApplicationContext(),tMsg , Toast.LENGTH_SHORT).show();
+        	Toast.makeText(this.getApplicationContext(),tMsg , Toast.LENGTH_SHORT).show();
         }
         setContentView(joystick);
         startComm();
@@ -157,10 +177,10 @@ public class ControlRemoteCar extends Activity {
 								
 								@Override
 								public void run() {
-									int x = joystick.userX;
-									int y = joystick.userY;
-									boolean xInBounds = (x > -128 && x < 128);
-									boolean yInBounds = (y > -128 && y < 128);
+									int x = joystick.getUserX();
+									int y = joystick.getUserY();
+									boolean xInBounds = (x > -127 && x < 127);
+									boolean yInBounds = (y > -127 && y < 127);
 									if (xInBounds && yInBounds){
 										byte drive = (byte) (y & 0x000000FF);
 										byte steering = (byte) (x & 0x000000FF);
--- trunk/0.1a/client/android/src/com/powerwheelino/JoystickClickedListener.java
+++
@@ -1,6 +0,0 @@
-package com.powerwheelino;
-
-public interface JoystickClickedListener {
-        public void OnClicked();
-        public void OnReleased();
-}
