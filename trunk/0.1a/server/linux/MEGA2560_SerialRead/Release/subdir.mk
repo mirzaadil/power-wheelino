41f0d617fe08df0f802e348b96344f5a5228dd9c
kranzrm <kranzrm@2fc9d784-2164-c497-5448-e10d180b2835>
2012-10-07 03:40:57 +0000

    Arduino Code updated, Servo control working \!\!\!

--- trunk/0.1a/server/linux/MEGA2560_SerialRead/Release/subdir.mk
+++ trunk/0.1a/server/linux/MEGA2560_SerialRead/Release/subdir.mk
@@ -4,12 +4,15 @@
 
 # Add inputs and outputs from these tool invocations to the build variables 
 CPP_SRCS += \
+../Servo.cpp \
 ../main.cpp 
 
 OBJS += \
+./Servo.o \
 ./main.o 
 
 CPP_DEPS += \
+./Servo.d \
 ./main.d 
 
 
@@ -17,7 +20,7 @@ CPP_DEPS += \
 %.o: ../%.cpp
 	@echo 'Building file: $<'
 	@echo 'Invoking: AVR C++ Compiler'
-	avr-g++ -I/usr/share/arduino/hardware/arduino/cores/arduino -I/usr/share/arduino/hardware/arduino/variants/mega -Wall -Os -ffunction-sections -fdata-sections -fno-exceptions -mmcu=atmega2560 -DF_CPU=16000000UL -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)" -c -o "$@" "$<"
+	avr-g++ -I/usr/share/arduino/hardware/arduino/cores/arduino -I/usr/share/arduino/libraries -I/usr/share/arduino/hardware/arduino/variants/mega -Wall -Os -ffunction-sections -fdata-sections -fno-exceptions -mmcu=atmega2560 -DF_CPU=16000000UL -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)" -c -o "$@" "$<"
 	@echo 'Finished building: $<'
 	@echo ' '
 
--- trunk/0.1a/server/linux/MEGA2560_SerialRead/main.cpp
+++ trunk/0.1a/server/linux/MEGA2560_SerialRead/main.cpp
@@ -1,14 +1,34 @@
 #include <Arduino.h>
+#include <Servo/Servo.h>
 
-// variables
-int ledPin = 13; // LED connected to digital pin 13
-char inData[16];
+//int ledPin = 13; // LED connected to digital pin 13
+
+//Serial Buffer
+char inData[4];
+
+//# of Bytes Read
 int bRead;
 
+//Hardware
+Servo steering;
+Servo drive;
+
+//Value Containers
+int rawSteeringVal;
+int rawDriveVal;
+int processedSteeringVal;
+int processedDriveVal;
+
 void setup(){
-	Serial.begin(57600); // opens serial port, sets data rate to 9600 bps
-	Serial.setTimeout(50000);
-	pinMode(ledPin, OUTPUT);      // sets the digital pin as output
+
+	Serial.begin(57600);	// Open serial port, set data rate to 57600 bps
+	Serial.setTimeout(100);	//Set serial timeout (ms)
+	//TODO: Tune this value !!!
+	//pinMode(ledPin, OUTPUT);	// sets the digital pin as output
+
+	steering.attach(5);		//Steering Servo Pin 5
+	drive.attach(6);		//Drive Servo (motor) Pin 6
+
 	Serial.println("------------------");
 	Serial.println("   Started");
 	Serial.println("------------------");
@@ -16,35 +36,35 @@ void setup(){
 
 void loop(void){
 
-	long before = millis();
     bRead = Serial.readBytesUntil(0x7F,inData,4);
-    if ( bRead == 0 ){
-    	Serial.println("Timeout");
-    	inData[0] = 0b00000000;
-    	inData[1] = 0b00000000;
+
+    if ( bRead != 2 ){	//Timeout or Noise
+    	Serial.print("! ");
+    	inData[0] = 0b00000000; //Steering
+    	inData[1] = 0b00000000; //Drive
+    } else { //2 bytes and Delimiter
+    	//Correct # of Bytes
+    	Serial.print("* ");
     }
-    if (bRead >3 ){
-    	Serial.println("Error");
-    }
 
-    Serial.write(27);
-    Serial.print("[2J"); // clear screen
-    Serial.write(27); // ESC
-    Serial.print("[H"); // cursor to home
+    rawSteeringVal = (int)inData[0];
+    rawDriveVal = (int)inData[1];
 
-    Serial.print("Bytes Read: ");
-    Serial.println(bRead);
-    Serial.println("First Byte");
-    Serial.println((int)inData[0]);
-    Serial.println(
-    		map((int)inData[0],0,126,0,1024)
-    );
+    //convert input range to desired angles
+    //using 60-120 provides 60 deg of movement L/R of center
+    processedSteeringVal = map(rawSteeringVal,-126,126,60,120);
+    processedDriveVal = map(rawDriveVal,-126,126,60,120);
 
-    Serial.println("Second Byte");
-    Serial.println((int)inData[1]);
-    Serial.println("");
-    Serial.println(millis() - before);
+    //Send to Hardware
+    steering.write(processedSteeringVal);
+    drive.write(processedDriveVal);
 
+    //Print Report
+    Serial.print(bRead);
+    Serial.print("\t");
+    Serial.print(processedSteeringVal);
+    Serial.print("\t");
+    Serial.println(processedDriveVal);
 
 }
 
